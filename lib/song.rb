b484c83007880a6b6ee7bc236ab6d857290d5f78
Kevin Boston <kbos22@gmail.com>
2020-05-26 21:01:16 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song 
+  attr_accessor :name, :artist 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    save 
+  end
+  def save 
+    @@all << self 
+  end
+  def self.all 
+    @@all 
+  end
+  def self.new_by_filename(name)
+    
+    
+    
+  end
+  def artist_name=(a_name)
+    
+    
+    a_name.add_song(song)
+  end
+  
+end
